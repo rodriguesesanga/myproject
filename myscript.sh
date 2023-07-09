987fad049ea358f6d7e46c075b2c91db9641bc8d
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-07-10 00:06:51 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -22,3 +22,5 @@ scp $1/./*.conf $2 >> script.log
 name=$(date '+%Y-%m-%d_%H-%M-%S%z') >> script.log
 tar -zcvf configure_files/confiles-$name.tar.gz $local_dir/ >> script.log
 
+..\..\..\azcopy_windows_amd64_10.19.0\azcopy.exe copy 'configure_files/confiles-$name.tar.gz' 'https://$storage[0].blob.core.windows.net/$storage[1]?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-07-10T05:47:15Z&st=2023-07-09T21:47:15Z&spr=https&sig=NqLPGKmPoxeauczwBB2BOx6%2FdYFNTDHaobGCX9A6VNc%3D'
+
