26cc651d5c564f0b1446aa2dfbc9142961186cca
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-07-09 22:28:20 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -24,5 +24,6 @@ tar -zcvf configure_files/confiles-$name.tar.gz $local_dir/ >> script.log
 
 #Upload tarball in Storage AZURE
 # shellcheck disable=SC2016
-.\azcopy_windows_amd64_10.19.0\azcopy.exe copy 'configure_files/confiles-$name.tar.gz' 'https://$storage[0].blob.core.windows.net/$storage[1]/confiles-$name.tar.gz' >> script.log
+cd c:
+azcopy_windows_amd64_10.19.0\azcopy.exe copy 'configure_files/confiles-$name.tar.gz' 'https://$storage[0].blob.core.windows.net/$storage[1]/confiles-$name.tar.gz' >> script.log
 
