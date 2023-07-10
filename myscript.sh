af3fad1ef88c3df086cdb7e464cd0c1b84be5149
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-07-10 12:19:44 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -16,14 +16,14 @@ echo "CONTAINER : ${storage[1]}" >> script.log
 #Get files from VM to local
 scp $1/./*.conf $2 >> script.log
 
+echo "listing files
+ls
 
 #Create tarball in local machine
-#IF [[ -z confiles.tar]] then,
 name=$(date '+%Y-%m-%d_%H-%M-%S%z') >> script.log
-tar -zcvf configure_files/confiles-$name.tar.gz $local_dir/ >> script.log
+tar -zcvf configure_files/confiles-$name.tar.gz $local_dir/
 
 ls configure_files
-confiles-2023-07-09_17-29-55+0200.tar.gz
 ../../../../../../azcopy_windows_amd64_10.19.0/azcopy.exe copy "configure_files/confiles-$name.tar.gz" "https://healthy.blob.core.windows.net/mycontainer?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-07-10T17:37:32Z&st=2023-07-10T09:37:32Z&spr=https&sig=gw5Ee9Z5gTMxTyq4Go7XHM7IhQXNLd2p9AY4r0aSFlk%3D"
 
 
